d2721326193e081559950d8ec267b8ff0a6d24e1
Martin M <35940368+progmartin@users.noreply.github.com>
2020-03-28 14:32:50 -0400

    commit 6

---
+++ src/dentist.delete.result.php
@@ -0,0 +1,99 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		include_once 'head/head.php';
+	?>
+</head>
+<body>
+
+
+
+	<?php
+		include_once 'header/header1.php';
+	
+		?>	
+		
+	<?php
+	?>	
+	
+
+<?php
+	include_once 'includes/dbh.inc.php';
+
+	$sqlDentist = "select * from dentist;";
+	$resultDentist = mysqli_query($conn, $sqlDentist);
+	$checkResultDentist = mysqli_num_rows($resultDentist);
+	
+	$sqlClinic = "select * from clinic;";
+	$resultClinic = mysqli_query($conn, $sqlClinic);
+	$checkResultClinic = mysqli_num_rows($resultClinic);
+	
+?>
+
+<br>
+<br>
+<div class = "middle">
+
+<form action="dentist.getinfo.result.php" method="post">
+	Get all dentist from 
+		<select name="selectDentist2">
+		  <option value="choose" selected="selected" >-- choose clinic --</option>
+		  <option value="*">all clinic</option>
+				
+		<?php
+			if ($checkResultClinic >0){
+					while ( $row = mysqli_fetch_array($resultClinic)){
+					
+						$tempCID =  $row['CID'];
+						$tempCName =  $row['clinicName'];
+						
+						echo "<option value='$tempCID'> $tempCID - $tempCName   </option>" 					
+					}				 				 
+				}
+		?>						
+		<input   type="submit" value="Search">
+		</select>
+</form> 
+<br>
+<br>
+<form action="dentist.getinfo.result.php" method="post">
+	Get all info on dentist 
+		<select  name="selectDentist1">
+	  <option value="choose" selected="selected" >-- choose dentist --</option>
+	  <option value="*">all dentist</option>
+	  
+	  
+		<?php
+			if ($checkResultDentist >0){
+					while ( $row = mysqli_fetch_array($resultDentist)){
+					
+						$tempID =  $row['EID'];
+						$tempName =  $row['name'];
+						$templastName =  $row['lastName'];
+						echo "<option value='$tempID'> $tempID - $tempName $templastName  </option>" ;					
+					}				 				 
+				}
+		?>
+	  
+	  <input type="submit" value="Search">
+	</select>
+</form> 
+
+</div>
+	
+	
+	
+	
+	
+	?>
+	
+	<?php
+		include_once 'footer/footer6php';
+	?>
+	
+
+</body>
+</html>
