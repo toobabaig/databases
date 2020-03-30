e68c1294100c9e2533e6f8830f2b5c50947adc61
Martin M <35940368+progmartin@users.noreply.github.com>
2020-03-30 16:17:43 -0400

    Add files via upload

---
+++ src/middle1.php
@@ -0,0 +1,12 @@
+
+
+<div class = "middle"><input type = "image" src="icons/logo.png"></div>
+<p class = "middle">
+Welcome to Clinic Manager!
+
+<br>
+<b><i>32 teeth, 1 dentist</i><b>
+</br>
+
+</p>
+
