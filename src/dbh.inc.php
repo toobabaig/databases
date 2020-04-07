1d44f88798e3582a5970b8dad9e4258fe3983c71
Martin M <35940368+progmartin@users.noreply.github.com>
2020-04-07 16:45:12 -0400

    db changed, old one expired

---
+++ src/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+	$dbServername = "remotemysql.com";
+	$dbUsername = "KFXxzGBmGy";
+	$dbPassword  = "2mnQ9zJcAt";
+	$dbName = "KFXxzGBmGy";
+
+	$conn = mysqli_connect($dbServername, $dbUsername ,$dbPassword ,$dbName);
+?>
+
